09315a7dc5838e41783738ed1408506ce6901e4b
Marina Tujimura <mtujimura@gmail.com>
2020-07-27 21:01:40 -0300

    Updates

---
+++ precache-manifest.bc821d2274c658d8893c0f4b703a7305.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c952998512b01e9342b14d3205634f26",
+    "url": "/github.io/marvell-app/index.html"
+  },
+  {
+    "revision": "eaedc14143cb2769ba32",
+    "url": "/github.io/marvell-app/static/css/main.60878f8f.chunk.css"
+  },
+  {
+    "revision": "f2eb9ca5d966196f210c",
+    "url": "/github.io/marvell-app/static/js/2.daf89e14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/marvell-app/static/js/2.daf89e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaedc14143cb2769ba32",
+    "url": "/github.io/marvell-app/static/js/main.bc55eaf7.chunk.js"
+  },
+  {
+    "revision": "3b3eb1316bb91f935090",
+    "url": "/github.io/marvell-app/static/js/runtime-main.ab9c1f53.js"
+  },
+  {
+    "revision": "22049e153648e058ea206112e24e6c25",
+    "url": "/github.io/marvell-app/static/media/heart-filled.22049e15.svg"
+  },
+  {
+    "revision": "61bd6eb829e1e3d6b7729042dfc84f62",
+    "url": "/github.io/marvell-app/static/media/heart.61bd6eb8.svg"
+  },
+  {
+    "revision": "529229ac3b4406539cf596297b447ee4",
+    "url": "/github.io/marvell-app/static/media/hero.529229ac.svg"
+  },
+  {
+    "revision": "55c7f3ac08d9ec989a6ca5d0833a96fb",
+    "url": "/github.io/marvell-app/static/media/logo.55c7f3ac.svg"
+  },
+  {
+    "revision": "aeefa6375ff61674da4b9c8cdb51faca",
+    "url": "/github.io/marvell-app/static/media/lupa.aeefa637.svg"
+  }
+]);
